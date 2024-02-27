3c34c05fba5d09b4c2994606c08d8d6c1ab0c8c6
Erick Casanova <casa.erickmr@gmail.com>
2024-02-26 19:38:48 -0600

    ::pencil2:: Se edita el sidebar y agrega view para las afiliacionesr

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,15 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AffiliationsFormView.vue')
-    }
+    },
+    {
+      path: '/searchaffiliations',
+      name: 'searchaffiliations',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/SerchAffiliationsView.vue')
+    },
   ]
 })
 
---
+++ src/views/SerchAffiliationsView.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import SearchAffiliations from '@/components/SearchAffiliations.vue';
+    import Sidebar from '@/components/Sidebar.vue';
+    import Navbar from '@/components/Navbar.vue';
+</script>
+<template>
+ <Navbar />
+<div class="flex overflow-hidden bg-white rounded-lg">
+    <Sidebar/>
+    <SearchAffiliations/>
+</div>
+</template>
