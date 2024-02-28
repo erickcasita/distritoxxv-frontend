e3c83841571e58c036a1aae9f601cd142e7b0278
Erick Casanova <casa.erickmr@gmail.com>
2024-02-27 19:01:42 -0600

    :sparkles: Se añade modal para la vista SearchAffiliations

--- src/components/SearchAffiliations.vue
+++ src/components/SearchAffiliations.vue
@@ -1,22 +1,25 @@
 <script setup>
-    import axios from 'axios'
-    import { ref } from 'vue'
+import axios from 'axios'
+import { ref } from 'vue'
+import ModalVieAffiliations from '@/components/ModalViewAffiliations.vue';
+  
+const modal = ref(false);
+const affiliations = ref([]);
 
-    const affiliations = ref([]);
-
-    const getAffiliations = async () => {
+const getAffiliations = async () => {
     await axios.get('https://distritoxxv-api.onrender.com/api/v1/affiliations')
         .then((response) => (
             affiliations.value = response.data,
             console.log("<<Sucess get affiliations>>", response.data.response, "status", response.status
             )))
         .catch((error) => (console.log(error.message)))
-    };
+};
 
-    getAffiliations();
+getAffiliations();
 
 </script>
 <template>
+     <ModalVieAffiliations v-show="modal"></ModalVieAffiliations>
     <div class="w-full max-w-screen-xl mx-auto px-4">
         <div class="bg-wite-300 dark:bg-gray-800 transition-colors duration-300">
             <div class="container mx-auto p-4">
@@ -24,29 +27,43 @@
                     <h1 class="text-xl font-semibold mb-4 text-gray-900 dark:text-gray-100">Búsqueda de Afiliaciones</h1>
                     <p class="text-gray-600 dark:text-gray-300 mb-6">Encuentre una nueva afiliación con base en el distrito
                         xxv.</p>
-                        <div class="rounded-lg border border-gray-200">
-                         <div class="overflow-x-auto rounded-t-lg">
-                    <table class="min-w-full divide-y-2 divide-gray-200 bg-white text-sm">
-                        <thead class="ltr:text-left rtl:text-right">
-                            <tr>
-                                <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Nombre</th>
-                                <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Comunidad / Colonia</th>
-                                <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Cargo</th>
-              
-                            </tr>
-                        </thead>
+                    <div class="rounded-lg border border-gray-200">
+                        <div class="overflow-x-auto rounded-t-lg">
+                            <table class="min-w-full divide-y-2 divide-gray-200 bg-white ">
+                                <thead class="ltr:text-left rtl:text-right">
+                                    <tr>
+                                        <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Nombre</th>
+                                        <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Comunidad /
+                                            Colonia</th>
+                                        <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Cargo</th>
+                                        <th class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Acciones</th>
 
-                        <tbody class="divide-y divide-gray-200">
-                            <tr v-for="affiliation in affiliations.data">
-                                <td class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">{{affiliation.name }}</td>
-                                <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{affiliation.town }}</td>
-                                <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{affiliation.rol }}</td>
-                            </tr>
-  
-                        </tbody>
-                    </table>
-                         </div>
+                                    </tr>
+                                </thead>
+
+                                <tbody class="divide-y divide-gray-200 ltr:text-left rtl:text-right">
+                                    <tr v-for="affiliation in affiliations.data">
+                                        <td class="whitespace-nowrap px-4 py-2 font-medium text-gray-900">{{ affiliation.name
+                                        }}</td>
+                                        <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{ affiliation.town }}</td>
+                                        <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{ affiliation.rol }}</td>
+                                        <td><span class="inline-flex overflow-hidden rounded-md border bg-white shadow-sm">
+                                                <button
+                                                    @click="modal = true"
+                                                    class="inline-block border-e p-3 text-gray-700 hover:bg-gray-50 focus:relative"
+                                                    title="Edit Product">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"></path><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z">   
+                                                        </path></svg>
+                                                </button>
+                                            </span>
+                                        </td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
                         </div>
+                    </div>
                 </div>
             </div>
 
