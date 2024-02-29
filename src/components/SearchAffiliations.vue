ad6b19e1f1d2f9bf6bc72407b3b23d88903e84bc
Erick Casanova <casa.erickmr@gmail.com>
2024-02-29 14:27:47 -0600

    :pencil2: Fix:  Se modifica modal con la informacion de cada afiliacion

--- src/components/SearchAffiliations.vue
+++ src/components/SearchAffiliations.vue
@@ -4,10 +4,46 @@ import { ref } from 'vue'
 import ModalVieAffiliations from '@/components/ModalViewAffiliations.vue';
   
 const modal = ref(false);
+const affiliationbyid = ref({
+    name: "",
+    lastname: "",
+    rol: "",
+    entity: "",
+    town: "",
+    postcode: null,
+    secction_vote: null,
+    phone_number: null,
+    address_home: ""
+});
 const cerrarModal = (value) =>{
+      
       modal.value = false;
+      affiliationbyid.value = {}
   }
 const affiliations = ref([]);
+
+
+
+const  getAfffiliationsById = async(id)=>{
+    console.log(id);
+    modal.value = true;
+    await axios.get('https://distritoxxv-api.onrender.com/api/v1/affiliations/'+id)
+        .then((response) => (
+            affiliationbyid.value.name =  response.data.details.name,
+            affiliationbyid.value.lastname =  response.data.details.last_name,
+            affiliationbyid.value.rol = response.data.details.rol,
+            affiliationbyid.value.entity = response.data.details.entity,
+            affiliationbyid.value.town = response.data.details.town,
+            affiliationbyid.value.postcode = response.data.details.postcode,
+            affiliationbyid.value.secction_vote = response.data.details.secction_vote,
+            affiliationbyid.value.phone_number = response.data.details.phone_number,
+            affiliationbyid.value.address_home = response.data.details.address_home,
+            console.log("<<Sucess get affiliations by id>>", response.data.response, "status", response.status
+            )))
+        .catch((error) => (console.log(error.message)))
+
+ 
+} 
 
 const getAffiliations = async () => {
     await axios.get('https://distritoxxv-api.onrender.com/api/v1/affiliations')
@@ -22,7 +58,7 @@ getAffiliations();
 
 </script>
 <template>
-     <ModalVieAffiliations :modal = "modal" @cerrarModal="cerrarModal"></ModalVieAffiliations>
+     
     <div class="w-full max-w-screen-xl mx-auto px-4">
         <div class="bg-wite-300 dark:bg-gray-800 transition-colors duration-300">
             <div class="container mx-auto p-4">
@@ -52,9 +88,9 @@ getAffiliations();
                                         <td class="whitespace-nowrap px-4 py-2 text-gray-700">{{ affiliation.rol }}</td>
                                         <td><span class="inline-flex overflow-hidden rounded-md border bg-white shadow-sm">
                                                 <button
-                                                    @click="modal = true"
+                                                    @click="getAfffiliationsById(affiliation._id)"
                                                     class="inline-block border-e p-3 text-gray-700 hover:bg-gray-50 focus:relative"
-                                                    title="Edit Product">
+                                                    title="Ver detalles de la afiliación">
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"></path><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z">   
                                                         </path></svg>
@@ -72,4 +108,5 @@ getAffiliations();
 
         </div>
     </div>
+    <ModalVieAffiliations  :modal = "modal"  :="affiliationbyid" @cerrarModal="cerrarModal"></ModalVieAffiliations>
 </template>
