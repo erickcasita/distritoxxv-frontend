d2c9f709244bdbbad4dfc308ff5b7061345dfc9e
Erick Casanova <casa.erickmr@gmail.com>
2024-03-02 15:53:52 -0600

    :pencil2: Add: Se agrego input de busqueda en la view searchs affiliations

--- src/components/SearchAffiliations.vue
+++ src/components/SearchAffiliations.vue
@@ -76,7 +76,16 @@ getAffiliations();
                     <h1 class="text-xl font-semibold mb-4 text-gray-900 dark:text-gray-100">Búsqueda de Afiliaciones</h1>
                     <p class="text-gray-600 dark:text-gray-300 mb-6">Encuentre una nueva afiliación con base en el distrito
                         xxv.</p>
-                    <div class="rounded-lg border border-gray-200">
+                        <div class="relative flex items-center mt-4 md:mt-0 py-8">
+                            <div class="absolute top-0 right-0">
+                            <button type="submit" class="absolute" data-svelte-h="svelte-rss0pz"><svg xmlns="http://www.w3.org/2000/svg" 
+                                fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 mx-3 text-gray-400 dark:text-gray-600  pt-3">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"> 
+                                </path></svg></button> 
+                                <input type="text" placeholder="Buscar cliente..." 
+                                class="block w-full py-1.5 pr-5  text-gray-700 bg-white border border-gray-300 rounded-lg md:w-80 placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"></div></div>
+                               
+                                <div class="rounded-lg border border-gray-200">
                         <div class="overflow-x-auto rounded-t-lg">
                            
                             <table class="min-w-full divide-y-2 divide-gray-200 bg-white ">
